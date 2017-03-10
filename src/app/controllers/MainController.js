e84b4c0e49d899d380506dbdd04501cf72665d4c
Kaashika R <Kaashika R>
2017-03-10 15:02:01 +0800

    added

--- src/app/controllers/MainController.js
+++ src/app/controllers/MainController.js
@@ -58,8 +58,8 @@
           var vm = this;
 
           vm.actions = [
-            { name: 'Share', icon: 'share', url: 'https://twitter.com/intent/tweet?text=Angular%20Material%20Dashboard%20https://github.com/flatlogic/angular-material-dashboard%20via%20@flatlogicinc' },
-            { name: 'Star', icon: 'star', url: 'https://github.com/flatlogic/angular-material-dashboard/stargazers' }
+            { name: 'Share', icon: 'share', url: 'https://github.com/ramachandraprabu/hacktohireteam3ui' },
+            { name: 'Star', icon: 'star', url: 'https://github.com/ramachandraprabu/hacktohireteam3ui' }
           ];
 
           vm.performAction = function(action) {
