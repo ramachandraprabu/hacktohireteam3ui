23b7cff88590b926950d83949686139f41035eb7
Kaashika R <Kaashika R>
2017-03-10 14:56:45 +0800

    progress

--- src/app/controllers/TableController.js
+++ src/app/controllers/TableController.js
@@ -12,7 +12,7 @@
     vm.showDialogue = showDialogue;
 
     vm.tableData = [];
-	tableFetch($http);
+	//tableFetch($http);
     tableService
       .loadAllItems()
       .then(function(tableData) {
@@ -29,14 +29,20 @@
 			  template: 
 				'<md-dialog style="padding: 20px 20px 20px 20px;" md-theme="mytheme">' +
 				'  <md-dialog-content>' +
-				'<h2>Call Details for '+dat.issue+'</h2>' +
-				'<p><b>Promotions Offered:</b>' +dat.status+'</p>'+
-				'<p><b>CDR Type:</b>' +dat.status+'</p>'+
-				'<p><b>IMEI:</b>' +dat.status+'</p>'+
-				'<p><b>IMSI:</b>' +dat.status+'</p>'+
-				'<p><b>Cause Termination Code:</b>' +dat.status+'</p>'+
-				'<p><b>Cause Termination Reason:</b>' +dat.status+'</p>'+
-				'<p><b>Location:</b>' +dat.status+'</p>'+
+				'<h2>CDR Type: '+dat.cdrType+'</h2>' +
+				'<p><b>IMEI:</b>' +dat.imei+'</p>'+
+				'<p><b>IMSI:</b>' +dat.imsi+'</p>'+
+				'<p><b>Calling Number:</b>' +dat.callingNumber+'</p>'+
+				'<p><b>Called Number:</b>' +dat.calledNumber+'</p>'+
+				'<p><b>Recording Entity:</b>' +dat.recordingEntity+'</p>'+
+				'<p><b>Location:</b>' +dat.location+'</p>'+
+				'<p><b>Call Duration:</b>' +dat.callDuration+'</p>'+
+				'<p><b>Answer Time:</b>' +dat.answerTime+'</p>'+
+				'<p><b>Seizure Time:</b>' +dat.seizureTime+'</p>'+
+				'<p><b>Release Time:</b>' +dat.releaseTime+'</p>'+
+				'<p><b>Cause For Termination:</b>' +dat.causeForTermination+'</p>'+
+				'<p><b>Basic Service:</b>' +dat.basicService+'</p>'+
+				'<p><b>MSC Address:</b>' +dat.mscAddress+'</p>'+				
 				'  </md-dialog-content>' +
 				'</md-dialog>'
 		}
